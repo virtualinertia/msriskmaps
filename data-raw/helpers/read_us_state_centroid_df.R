de6cd9bb3ddd3bdd4c56565247202e6eb76ca0a1
virtualinertia <alex.karman@virtualinertiaailabs.com>
2024-08-21 02:39:20 +0000

    Docs for US State Centroids

--- data-raw/helpers/read_us_state_centroid_df.R
+++ data-raw/helpers/read_us_state_centroid_df.R
@@ -6,3 +6,5 @@ us_state_centroid_df <- readRDS("data-raw/helpers/us_state_centroid_df.rds")
 usethis::use_data(us_state_centroid_df, overwrite = TRUE)
 
 usethis::use_r("us_state_centroid_df")
+
+devtools::document()

7ff39f5d198b5359834d613683a4fac726c11881
virtualinertia <alex.karman@virtualinertiaailabs.com>
2024-09-11 19:24:55 +0000

    Documented Lidar extents

---
+++ R/ms_lidar_sdf.R
@@ -0,0 +1,13 @@
+#' @title Mississippi Lidar Point Cloud Extents
+#'
+#' @description Mississippi Lidar Point Cloud Extents
+#' for Natchez Trace (2016) .
+#'
+#' @format SF tibble. 5 POLYGON MBRs in Warren County.
+#'
+#'
+#' @details Non-spatial columns contain USGS 3DEP metadata
+#'
+#' @source Included
+#'
+"ms_lidar_sdf"
